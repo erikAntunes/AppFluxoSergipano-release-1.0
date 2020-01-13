92e1ad7d08ce0dc2f96b1c9233f2a94ec8f07292
Erik Antunes <erik.antunes@live.com>
2020-01-13 16:09:18 -0300

    navegação pronta

--- app/src/main/java/com/titanz/fluxosergipano/MainActivity.java
+++ app/src/main/java/com/titanz/fluxosergipano/MainActivity.java
@@ -1,14 +1,41 @@
 package com.titanz.fluxosergipano;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.titanz.fluxosergipano.adapters.MenuAdapter;
+import com.titanz.fluxosergipano.view.EntradaFragment;
+import com.titanz.fluxosergipano.view.SaidaFragment;
+import com.titanz.fluxosergipano.view.TotalFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        tabLayout = findViewById(R.id.tablayout_main_id);
+        viewPager = findViewById(R.id.viewpager_main_id);
+
+
+        //Adicionando Fragments
+
+        MenuAdapter adapter = new MenuAdapter(getSupportFragmentManager());
+        adapter.AddFragment(new EntradaFragment(),"Entrada" );
+        adapter.AddFragment(new SaidaFragment(),"Saída" );
+        adapter.AddFragment(new TotalFragment(),"Total" );
+
+        // Adapter Setup
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/titanz/fluxosergipano/adapters/MenuAdapter.java
@@ -0,0 +1,39 @@
+package com.titanz.fluxosergipano.adapters;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentListTitles = new ArrayList<>();
+
+
+    public MenuAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentListTitles.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentListTitles.get(position);
+    }
+
+    public void AddFragment (Fragment fragment, String title){
+        fragmentList.add(fragment);
+        fragmentListTitles.add(title);
+    }
+}
